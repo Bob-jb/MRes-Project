edbaba219d85f5478200736b2a8572645cdc37ac
Bobby <robert.banks.20@ucl.ac.uk>
2021-05-18 15:29:15 +0100

    Plot ising

--- ising.py
+++ ising.py
@@ -2,7 +2,7 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def generate(adj):
+def gen_ising(adj):
     #Create a hardware native spin glass of the form 
     # \sum_{i,j} J_{i,j} Z_i Z_j with J_{i,j} being 1 or -1 
     # from a numpy array or list of lists. Returns a numpy array
@@ -16,6 +16,18 @@ def generate(adj):
 
 def plot_ising(adj):
     # Create plot of Ising model
-     G=nx.from_numpy_matrix(adj)
+    
+    G=nx.from_numpy_array(adj)
+    pos = nx.spring_layout(G, seed=63)
+    nx.draw(G, pos, with_labels=True, font_weight='bold')
+    nx.draw_networkx_edge_labels(G, pos)
+    plt.show()
+
+def solve_ising(adj):
+    #Solve Ising 
 
 
+mat=[[0,1],[1,0]]
+y=gen(mat)
+print(y)
+plot_ising(y)
